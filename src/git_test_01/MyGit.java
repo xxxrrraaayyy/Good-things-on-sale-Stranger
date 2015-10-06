8a09f852d142130519c24849cc5f3284c7eade39
xxxrrraaayyy <tongbaxu@gmail.com>
2015-10-06 18:46:36 -0400

    First try

---
+++ src/git_test_01/MyGit.java
@@ -0,0 +1,12 @@
+package git_test_01;
+
+public class MyGit {
+	
+	public static void main(String[] args) {
+		for(int i=0; i<3; i++) {
+			System.out.println("Greetings, My Lord!");
+		}
+		System.out.println("Mom says the important thing should be repeated three times.");
+	}
+
+}
